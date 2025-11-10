4dd200f6349c5b20edd4ffd3b2ddeae565d1df41
IvanF <filipovicivan8@gmail.com>
2025-11-10 14:34:46 +0100

    Environment setup

---
+++ TheSampleApi/Endpoints/RootEndPoints.cs
@@ -0,0 +1,9 @@
+﻿namespace TheSampleApi.Endpoints;
+
+public static class RootEndPoints
+{
+    public static void AddRootEndpoints(this WebApplication app)
+    {
+        app.MapGet("/", () => "Hello World");
+    }
+}
---
+++ TheSampleApi/Program.cs
@@ -0,0 +1,16 @@
+using TheSampleApi.Startup;
+using TheSampleApi.Endpoints;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddDependencies();
+
+var app = builder.Build();
+
+app.UseOpenApi();
+
+app.UseHttpsRedirection();
+
+app.AddRootEndpoints();
+
+app.Run();
